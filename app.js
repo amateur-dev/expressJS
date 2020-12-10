f0031460fd82ce94856005ce37a3cc4318ae33c9
Dipesh Sukhani <me@dipeshsukhani.dev>
2020-12-11 07:15:41 +0800

    expressJS assignment

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res, next) => {
-    console.log("this is middleware1");
-    next();
+app.get('/users', (req, res) => {
+    res.send({ userName: "Dipesh" });
 })
 
-app.use((req, res, next) => {
-    console.log("this is middleware2");
-    next();
+app.get('/', (req, res) => {
+    res.send("Hi there!");
 })
 
-app.use((req, res) => {
-    res.send('Hello World from the last block');
-})
+// app.get('/', (req, res, next) => {
+//     console.log("this is middleware1");
+//     next();
+// })
+
+// app.use((req, res, next) => {
+//     console.log("this is middleware2");
+//     next();
+// })
+
+// app.use((req, res) => {
+//     res.send('Hello World from the last block');
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
