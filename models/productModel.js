6964f6611ef1f6fd51949f8b70418962d1581ce4
Dipesh Sukhani <me@dipeshsukhani.dev>
2020-12-15 09:48:02 +0800

    refactored the model

--- models/productModel.js
+++ models/productModel.js
@@ -3,39 +3,38 @@ const fs = require('fs');
 const path = require('path');
 const pathUtil = require('../utils/path');
 
+const path2file = path.join(pathUtil, "data", "products.json");
+
+const goGetProducts = (cb) => {
+    fs.readFile(path2file, (err, fileContent) => {
+        if (err) {
+            console.log(`There is an error in reading the file from the fetchAll function ${err}`);
+            return cb([])
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
+
 const ProductModel = class Product {
     constructor(t) {
         this.title = t;
         this.save()
     }
-
     save() {
-
         try {
-            const path2file = path.join(pathUtil, "data", "products.json");
-            fs.readFile(path2file, (err, fileContent) => {
-                let products = [];
-                if (!err) {
-                    products = JSON.parse(fileContent);
-                }
+            goGetProducts((products) => {
                 products.push(this);
                 fs.writeFile(path2file, JSON.stringify(products), (err) => {
-                    console.error(err);
+                    if (err == null) { console.error(`This is the ${err}`) };
                 });
             })
-
         } catch (err) {
             console.error(err);
         }
-
     }
 
     static fetchAll(cb) {
-        fs.readFile(path.join(pathUtil, "data", "products.json"), (err, fileContent) => {
-            if (err) { cb([]) }
-            cb(JSON.parse(fileContent));
-        });
-
+        goGetProducts(cb);
     }
 }
 
